6573e1f844cc29a432ef267b4a1f82ba8d6a14cc
serggunnya <baranov.serg.it@gmail.com>
2025-07-16 18:20:50 +0500

    delete layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,21 @@
 import { Metadata } from "next";
-import MainLayout from "./(shop)/layout";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "404",
-  description: "Страница не найдена",
+	title: "404",
+	description: "Страница не найдена",
 };
 
 export default function NotFound() {
-  return (
-    <MainLayout>
-    <div className="flex items-center justify-center absolute w-full h-full">
-      <h1 className="font-bold">Страница не найдена</h1>      
-    </div>
-    </MainLayout>
-  );
+	return (
+		<div className="flex items-center justify-center absolute w-full h-full flex-col gap-4">
+			<h1 className="font-bold text-2xl">Страница не найдена</h1>
+			<Link
+				href="/"
+				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+			>
+				На главную
+			</Link>
+		</div>
+	);
 }
