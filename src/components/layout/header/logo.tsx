b6117dac7d8e7057a51ac3d055aca4844dbaf95a
serggunnya <baranov.serg.it@gmail.com>
2025-07-16 18:21:10 +0500

    add Link

--- src/components/layout/header/logo.tsx
+++ src/components/layout/header/logo.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Logo() {
 	return (
 		<div className="">
-			<a
+			<Link
+				href={"/"}
 				className="text-white text-3xl mr-[10%] font-extrabold text-shadow-[4px_3px_2px_rgb(25_25_25_/_0.50)]"
-				href="/"
 			>
 				Shoppirest
-			</a>
+			</Link>
 		</div>
 	);
 }
