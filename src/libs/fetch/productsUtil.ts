8a5fb46680863b262ee752e3c99237fd2328c9d3
serggunnya <baranov.serg.it@gmail.com>
2025-08-04 17:23:12 +0500

    add fetch util for ssr

---
+++ src/libs/fetch/productsUtil.ts
@@ -0,0 +1,57 @@
+import { IFacet, IFiltersBody, ISearchResponse } from "@/types/products.interface";
+
+const ProductsUtil = {
+	fetchProducts: async (
+		params: URLSearchParams,
+		filters: IFiltersBody,
+	): Promise<ISearchResponse> => {
+		const url = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/products/search?${params}`;
+
+		try {
+			const response = await fetch(url, {
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				credentials: "include",
+				method: "POST",
+				body: JSON.stringify(filters),
+				cache: "no-cache",
+			});
+
+			if (!response.ok) {
+				throw new Error(`Failed to fetch data. Status: ${response.status}`);
+			}
+
+			return await response.json();
+		} catch (error) {
+			throw new Error("Could not fetch products and facets. Please check the API connection.");
+		}
+	},
+
+	fetchFacets: async (params: URLSearchParams, filters: IFiltersBody): Promise<IFacet[]> => {
+		const url = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/products/facets?${params}`;
+
+		try {
+			const response = await fetch(url, {
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				method: "POST",
+				body: JSON.stringify(filters),
+				cache: "no-cache",
+			});
+
+			if (!response.ok) {
+				throw new Error(`Failed to fetch data. Status: ${response.status}`);
+			}
+
+			return await response.json();
+		} catch (error) {
+			throw new Error("Could not fetch products and facets. Please check the API connection.");
+		}
+	},
+};
+
+export default ProductsUtil;
